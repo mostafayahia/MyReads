eb10a7c0992cdf9bae2a59ba341210230491df3d
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2021-11-17 16:28:57 +0200

    feat: fetching books for 'want to read' & 'read' cateogries from api

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,7 @@ const Book = ({ title, authors, imageURL }) => (
                 <BookShelfChanger />
             </div>
             <div className="book-title">{title}</div>
-            <div className="book-authors">{authors}</div>
+            <div className="book-authors">{authors.join(', ')}</div>
         </div>
     </li>
 );
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,9 +4,11 @@ import BookList from './BookList';
 const BookShelf = ({ title, books }) => (
     <div className="bookshelf">
         <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-            <BookList books={books} />
-        </div>
+        {
+            books && books.length ? <div className="bookshelf-books">
+                <BookList books={books} />
+            </div> : <p>No Books in this shelf yet.</p>
+        }
     </div>
 );
 
